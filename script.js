0037e03ed0e5126808226903ed465a765b8e0aae
priitrs <priitrs@gmail.com>
2023-09-25 09:30:47 +0300

    add all stops

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
 let stops = {
     "Hagudi" : "64-6298-5",
-    "Tallinn" : "64-6298-93"
+    "Liiva": "64-6298-48",
+    "TallinnW" : "64-6298-93",
+    "TallinnS" : "64-6320-93",
+    "Ulemiste": "64-6320-117"
 }
 
-const toTallinnButton = document.getElementById('data');
+const hagudiTallinn = document.getElementById('hagTal');
+const tallinnUlemiste = document.getElementById('talUle');
+const ulemisteTallinn = document.getElementById('uleTal');
+const tallinnHagudi = document.getElementById('talHag');
+const liivaHagudi = document.getElementById('liivaHag');
 
-function getMinutesFromMidnight() {
-    const currentDate = new Date();
-    const currentTime = currentDate.getTime();
-    currentDate.setHours(0, 0, 0, 0);
-    return Math.round((currentTime - currentDate.getTime()) / (1000 * 60));
-}
+hagudiTallinn.addEventListener(('click'), () => {
+    getTimesForJourney(stops.Hagudi, stops.TallinnW, hagudiTallinn);
+});
 
-toTallinnButton.addEventListener(('click'), () => {
-    fetchData(stops.Hagudi, stops.Tallinn).then(res => {
+tallinnHagudi.addEventListener(('click'), () => {
+    getTimesForJourney(stops.TallinnW, stops.Hagudi, tallinnHagudi);
+});
+
+ulemisteTallinn.addEventListener(('click'), () => {
+    getTimesForJourney(stops.Ulemiste, stops.TallinnS, ulemisteTallinn);
+});
+
+liivaHagudi.addEventListener(('click'), () => {
+    getTimesForJourney(stops.Liiva, stops.Hagudi, liivaHagudi);
+});
+
+tallinnUlemiste.addEventListener(('click'), () => {
+    getTimesForJourney(stops.TallinnS, stops.Ulemiste, ulemisteTallinn);
+});
+
+function getTimesForJourney(start, end, journeyNode) {
+    fetchData(start, end).then(res => {
         console.log(res);
         const currentMinutesFromMidnight = getMinutesFromMidnight();
 
+        const allTrips = document.querySelectorAll('.trip');
+        allTrips.forEach(trip => {
+            while (trip.firstChild) {
+                trip.removeChild(trip.firstChild);
+            }
+        });
+        
         res.forEach(trip => {
             let tripData = trip.trips[0]
             if (tripData.departure_time_min > currentMinutesFromMidnight) {
@@ -24,14 +51,21 @@ toTallinnButton.addEventListener(('click'), () => {
                 let arrival = new Date(tripData.arrival_time);
                 const listItem = document.createElement('p')
                 listItem.innerText = departure.toLocaleTimeString() + ' - ' + arrival.toLocaleTimeString();
-                toTallinnButton.appendChild(listItem)
+                journeyNode.appendChild(listItem)
                 // console.log(departure.toLocaleTimeString() + ' - ' + arrival.toLocaleTimeString())
             }
         });
     }).catch(e => {
         console.log(e)
     });
-});
+}
+
+function getMinutesFromMidnight() {
+    const currentDate = new Date();
+    const currentTime = currentDate.getTime();
+    currentDate.setHours(0, 0, 0, 0);
+    return Math.round((currentTime - currentDate.getTime()) / (1000 * 60));
+}
 
 function fetchData(originStop, destinationStop) {
     return fetch('https://api.ridango.com/v2/64/intercity/stopareas/trips/direct', {
@@ -41,7 +75,7 @@ function fetchData(originStop, destinationStop) {
             'Accept': 'application/json, text/plain, */*',
         },
         body: JSON.stringify({
-            "date": "2023-09-22",
+            "date": getCurrentDate,
             "origin_stop_area_id": originStop,
             "destination_stop_area_id": destinationStop,
             "channel": "web"
@@ -69,4 +103,12 @@ function fetchData(originStop, destinationStop) {
         .catch(error => {
             console.error('Fetch error:', error);
         });
+}
+
+function getCurrentDate() {
+    const year = currentDate.getFullYear(); // Get the current year
+    const month = currentDate.getMonth();    // Get the current month (0-11, where 0 is January)
+    const day = currentDate.getDate();       // Get the current day of the month (1-31)
+
+    return `${year}-${month + 1}-${day}`;
 }
